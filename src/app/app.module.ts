e1cb42e9f53b43aae6eb6ebd9c6277647a347df6
shanajasila <shanajasilam@gmail.com>
2022-11-29 12:03:35 +0530

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewallCourseComponent } from './viewall-course/viewall-course.componen
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 
 const myRoute : Routes =[
 {
@@ -31,7 +32,8 @@ const myRoute : Routes =[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
